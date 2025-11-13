de82ac334f9ccf342e911002e23f7683c878f963
Gabriel Mass <contato.gxbemass@gmail.com>
2025-11-13 02:07:37 -0300

    Ajustando porta para Render

--- server.js
+++ server.js
@@ -221,6 +221,8 @@ async function enviarMensagemWhatsApp(telefone, texto) {
 // ===============================================
 // 🔥 SERVIDOR
 // ===============================================
-app.listen(3000, () => {
-    console.log("🔥 Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
